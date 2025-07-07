58897bf6f5c887529b4874d2367d1295ef37df39
JonathanJourney99 <ijonathan.gomes@gmail>
2025-07-07 22:35:28 +0530

    added:r_insert

--- Leetcode-Python/Tree/tree.py
+++ Leetcode-Python/Tree/tree.py
@@ -47,9 +47,9 @@ class BinarySearchTree:
         return False
     # Recursive method for contains()
     def _r_contains(self, current_node, value):
-        if current_node == None:
+        if current_node == None: # node value is None
             return False
-        if value == current_node.value:
+        if value == current_node.value: # got the node
             return True
         if value < current_node.value:
             return self._r_contains(current_node=current_node.left, value=value)
@@ -63,20 +63,18 @@ class BinarySearchTree:
     def _r_insert(self, current_node:Node, value):
         if current_node == None:
             return Node(value)
-        if value < current_node.value:
+        if value < current_node.value:  
             current_node.left = self._r_insert(current_node.left, value)
         if value > current_node.value:
             current_node.right = self._r_insert(current_node.right, value)
         return current_node
-
+    
     # a func() to handle recursive insert()
     def r_insert(self, value):
         if self.root == None:
             self.root = Node(value)
         self._r_insert(self.root, value)
 
-            
-            
 tree = BinarySearchTree()
 tree.insert(47)
 tree.insert(21)
@@ -90,3 +88,15 @@ print(tree.contains(27)) # True
 print(tree.contains(8)) # False
 
 print(tree.r_contains(27)) # True
+
+tree1 = BinarySearchTree()
+tree1.r_insert(2)
+tree1.r_insert(1)
+tree1.r_insert(3)
+
+print(tree1.r_contains(1)) # True
+
+print(" Root: ", tree1.root.value)
+print(" Root -> Left: ", tree1.root.left.value)
+print(" Root _> Right: ", tree1.root.right.value)
+
